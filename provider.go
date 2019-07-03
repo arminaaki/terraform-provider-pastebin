49da470105acb385563b70738989884c2480feb0
Armin <armin.ak89@gmail.com>
2019-07-02 18:09:22 -0700

    Define basic provider config

---
+++ provider.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"api_dev_key": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The api_dev_key for API operations.",
+			},
+			"api_user_name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The api_user_name for API operations.",
+			},
+			"api_user_password": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The api_user_password for API operations.",
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{},
+
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	config := Config{
+		ApiUserName:     d.Get("api_user_name").(string),
+		ApiUserPassword: d.Get("api_user_password").(string),
+		ApiDevKey:       d.Get("api_dev_key").(string),
+		BaseUrl:         "https://pastebin.com/",
+	}
+
+	return &config, nil
+}
