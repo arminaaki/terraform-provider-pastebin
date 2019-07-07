f1dec4e19fffe7a0a1191ea1ac5637a411138966
arminaaki <armin.ak89@gmail.com>
2019-07-06 19:36:32 -0700

    Add pastebin_create_paste resource

--- examples/main.tf
+++ examples/main.tf
@@ -12,14 +12,13 @@ output "API KEY" {
   value = "${pastebin_api_user_key.api_key.api_user_key}"
 }
 
-resource "pastebin_create_api" "pasteA" {
-  api_dev_key           =  "" #'2cddd4dab41f10754e9dfd5dd6f9fbbf'; // your api_developer_key
-  api_paste_code        =  "" #'just some random text you :)'; // your paste text
-  api_paste_private     =  "" #'1'; // 0=public 1=unlisted 2=private
-  api_paste_name        =  "" #'justmyfilename.php'; // name or title of your paste
-  api_paste_expire_date =  "" #'10M';
-  api_paste_format      =  "" #'php';
-  api_user_key          =  "" #''; // if an invalid or expired api_user_key is used, an error will spawn. If no api_user_key is used, a guest paste will be created
-  api_paste_name        =  "" #urlencode($api_paste_name);
-  api_paste_code        =  "" #urlencode($api_paste_code);
+resource "pastebin_create_paste" "pasteA" {
+  name                  = "pasteA"
+  api_user_key          = "${pastebin_api_user_key.api_key.api_user_key}" #''; // if an invalid or expired api_user_key is used, an error will spawn. If no api_user_key is used, a guest paste will be created
+  api_dev_key           = "${var.api_dev_key}" #'2cddd4dab41f10754e9dfd5dd6f9fbbf'; // your api_developer_key
+  api_paste_code        = "just some random text you :)" #'just some random text you :)'; // your paste text
+  api_paste_private     = "1" #'1'; // 0=public 1=unlisted 2=private
+  api_paste_name        = "justmyfilename.php"
+  api_paste_expire_date = "10M"
+  api_paste_format      = "php"
 }
